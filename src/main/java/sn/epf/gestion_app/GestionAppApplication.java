29caee134492e7191247e71300b0eb41a32081fe
Khoudia Niang <niang62@yahoo.com>
2026-06-18 02:29:24 +0000

    init: setup Spring Boot project - Module 1

---
+++ src/main/java/sn/epf/gestion_app/GestionAppApplication.java
@@ -0,0 +1,13 @@
+package sn.epf.gestion_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/epf/gestion_app/GestionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.epf.gestion_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
